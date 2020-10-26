68c31b2c645b4a59be6f54153180b85677a05998
igor.petrenko <igor.petrenko@xenoss.io>
2020-10-26 14:08:31 +0200

    fix: build

--- start.sh
+++ start.sh
@@ -8,11 +8,11 @@ else
   cpuCount=$(($(nproc) / 2))
 fi
 
-echo "exec java \
+exec java \
   @conf/vm.options \
   -XX:ActiveProcessorCount=${cpuCount} \
   -cp $1/conf:lib/* \
   oap.application.Boot \
   --start \
   --config-directory=/etc/xenoss/conf.d \
-  --config=conf/application.conf"
+  --config=conf/application.conf
