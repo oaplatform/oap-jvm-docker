f6f64496d95cef88bef685c876ba0189014d6352
igor.petrenko <igor.petrenko@xenoss.io>
2021-10-28 09:07:39 +0300

    jdk17: fix cmd

--- start.sh
+++ start.sh
@@ -67,7 +67,6 @@ exec ${CMD} \
   --add-opens=java.base/java.net=ALL-UNNAMED \
   --add-opens=java.base/java.text=ALL-UNNAMED \
   --add-opens=java.sql/java.sql=ALL-UNNAMED \
-  --add-opens=java.util.concurrent.atomic/java.util.concurrent.atomic=ALL-UNNAMED \
   -XX:ActiveProcessorCount=${cpuCount} \
   -cp $1/conf:lib/* \
   oap.application.Boot \
